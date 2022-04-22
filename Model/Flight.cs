b7aa7440b1bbed9bd27e1e00aa49f1673dadcee9
Лиза Иванкова <kvpsgr@mail.ru>
2022-04-22 23:48:03 +0700

    new

---
+++ Model/Flight.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programming.Model
+{
+    public class Flight
+    {
+        private int _flightTime;
+
+        public Flight()
+        {
+
+        }
+        public Flight(string departurePoint, string destinationPoint,int flightTime)
+        {
+            FlightTime = flightTime;
+            DeparturePoint = departurePoint;
+            DestinationPoint = destinationPoint;
+        }
+        public string DeparturePoint { get; set; }
+
+        public string DestinationPoint { get; set; }
+
+        public int FlightTime
+        {
+            get
+            {
+                return _flightTime;
+            }
+            set
+            {
+                if(value < 0)
+                {
+                    throw new ArgumentException("");
+                }
+                _flightTime = value;
+            }
+        }
+    }
+}
